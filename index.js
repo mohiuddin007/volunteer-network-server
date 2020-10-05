08491db21d6711ce7e9f3a18cc7430768d55a892
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-10-05 19:18:59 +0600

    last commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l47bs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
@@ -67,6 +66,5 @@ client.connect(err => {
       })
     })
 });
-
 
 app.listen(process.env.PORT || 8000)
